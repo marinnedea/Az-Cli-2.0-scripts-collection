edf8ca90be32a309324dcd8ac55fa6946ff857b8
Marin Nedea <marinnedea@users.noreply.github.com>
2018-08-22 16:58:21 +0300

    Update diskutils.sh

--- diskutils.sh
+++ diskutils.sh
@@ -246,7 +246,7 @@ scan_partition_format()
 	    MOUNTPOINT=$(get_next_mountpoint)
 	    echo "Next mount point appears to be ${MOUNTPOINT}"
 	    [ -d "${MOUNTPOINT}" ] || mkdir -p "${MOUNTPOINT}"
-	    read UUID FS_TYPE <<< (blkid -u filesystem ${PARTITION}|awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+	    read UUID FS_TYPE <<< $(blkid -u filesystem ${PARTITION}|awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
 	    add_to_fstab "${UUID}" "${MOUNTPOINT}"
 	    echo "Mounting disk ${PARTITION} on ${MOUNTPOINT}"
 	    mount "${MOUNTPOINT}"
