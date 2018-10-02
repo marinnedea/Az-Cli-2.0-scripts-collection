5e18ae4784517dca56bc786d501569c4e6b25f7f
Marin Nedea <marinnedea@users.noreply.github.com>
2018-10-02 18:19:18 +0300

    Create create_an_vms.sh

---
+++ create_an_vms.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+#############################
+# Optional
+# Login into Azure
+#az login
+
+# If multiple subscription, set the one where VM and KeyVault are.
+#az account set -s "<YourSubscriptionID>"
+# END Optional
+#############################
+
+# Decide on a region for the resources to be created
+region="Set_region_here"
+
+# Set a resource group name and create it
+rgName="antestrg1"
+az group create --name $rgName --location $region
+
+# Use a specific username
+userName="azureuser"
+
+# Set the network parametters:
+ANVnet="myANVnet"
+ANSubnet="myANSubnet"
+ANNSG="myANNSG"
+nsgruleName="Allow-SSH-Internet"
+
+# Create a virtual network with az network vnet create. The following example creates a virtual network named myVnet with one subnet
+az network vnet create -g $rgName --name $ANVnet --address-prefix 192.168.0.0/16 --subnet-name $ANSubnet --subnet-prefix 192.168.1.0/24
+
+# Create a network security group
+az network nsg create -g $rgName --name $ANNSG
+
+# Open a port to allow SSH access to the virtual machine with az network nsg rule create:
+az network nsg rule create -g $rgName --nsg-name $ANNSG --name $nsgruleName --access Allow --protocol Tcp --direction Inbound --priority 100 --source-address-prefix Internet --source-port-range "*"  --destination-address-prefix "*" --destination-port-range 22
+
+# Simulate a multidimensional array for the VMs we need to create, each subarray containing the VM name and Linux Image URN/SKU
+declare -a vms
+
+vms[0]='UbuntuAN1;Canonical:UbuntuServer:16.04.0-LTS:16.04.201808140;1'
+vms[1]='SLESAN1;SUSE:SLES:12-SP3:2018.09.04;2'
+vms[2]='RHELAN1;RedHat:RHEL:7.4:7.4.2018010506;3'
+vms[3]='CentOSAN1;OpenLogic:CentOS:7.4:7.4.20180704;4'
+vms[4]='CoreOSAN1;CoreOS:CoreOS:Stable:1855.4.0;5'
+vms[5]='DebianAN1;credativ:Debian:9:9.0.201808270;6'
+vms[6]='OracleUEKAN1;Oracle:Oracle-Linux:7.4:7.4.20180424;7'
+vms[7]='OracleRHCKAN1;Oracle:Oracle-Linux:7.4:7.4.20180424;8'
+
+# I used the same image for Oracle.
+# Will switch the boot order after creating them in order to get one booting the RHCK kernel.
+
+for i in "${vms[@]}"
+do
+
+        arr=(${i//;/ })
+        vmName=${arr[0]}
+        vmImage=${arr[1]}
+        c=${arr[2]}
+#echo $vmName;
+#echo $vmImage;
+#echo "$c"
+# Create a public IP address with az network public-ip create.
+# A public IP address isn't required if you don't plan to access the virtual machine from the Internet, so you may skip this step.
+az network public-ip create --name ANPubIp$c -g $rgName
+
+# Create a network interface with az network nic create with accelerated networking enabled.
+az network nic create  -g $rgName --name ANNic$c --vnet-name $ANVnet --subnet $ANSubnet --accelerated-networking true --public-ip-address ANPubIp$c --network-security-group $ANNSG
+
+# Create the VM and attach the NIC you just created to the VM:
+az vm create -g $rgName -n $vmName --image $vmImage --size Standard_DS4_v2 --admin-username $userName --generate-ssh-keys --nics ANNic$c
+
+done
