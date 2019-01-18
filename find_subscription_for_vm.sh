1f49faea7d960495488328c6937dc434293b4d7f
Marin Nedea <marinnedea@users.noreply.github.com>
2019-01-18 16:51:18 +0200

    Update find_subscription_for_vm.sh

--- find_subscription_for_vm.sh
+++ find_subscription_for_vm.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env bash
 
-#replace myVm with your actual VM name
+# The script will run only under bash, as it is using arrays!! 
+# sh/zsh doesn't understand the "declare" function.
+# To run the script, use the following command:
+# chmod +x find_subscription_for_vm.sh && ./find_subscription_for_vm.sh
+# Enjoy!
+
+#Type the Vm name when prompted
 read -p 'Type the Vm name to search: ' findvmname
 
 # List all subscriptions you have access and are enabled
@@ -8,30 +14,29 @@ declare -a accarray="$(az account list --all --query "[].id" -o tsv)"
 
 #check if array is empty
 if [ -z "$accarray" ]; then
-    echo "No subscription enabled on this Azure account"
-	exit 0
+    echo "No subscription enabled on this account"
+    exit 0
 else
-	for i in ${accarray[@]};  do
-		
-	subid=$i;
-	
-	# List all VMs in subscription $subid
-	declare -a vmarray="$(az vm list --subscription $subid --query "[].name" -o tsv)"
-	
-	#check if array is empty
-		if [ -z "$vmarray" ]; then
-				echo "No VM in subscription "$subid 
-			else
-				for j in ${vmarray[@]}; do
-				vmName=$j;
-				
-				#Check if the VM name you search is present
-				if [[ "$vmName" == "$findvmname" ]]; then
-					echo "VM $vmName is in Subscription "$subid 
-					exit 0
-				else		
-					echo "VM $vmName NOT found in Subscription "$subid	
-				fi	
-			fi
-	done; 	
+        for i in ${accarray[@]};  do
+
+        subid=$i;
+
+        # List all VMs in subscription $subid
+        declare -a vmarray="$(az vm list --subscription $subid --query "[].name" -o tsv)"
+
+        #check if array is empty
+        if [ -z "$vmarray" ]; then
+                echo "No VM in subscription "$subid
+        else
+                for j in ${vmarray[@]}; do
+                        vmName=$j;
+
+                        #Check if the VM name you search is present
+                        if [[ "$vmName" == "$findvmname" ]]; then
+                                echo "VM $findvmname is in Subscription "$subid
+                                exit 0
+                        fi
+                done;
+        fi
+        done;
 fi
